0d9cc8c191412729b5fa1ae153151659319534fb
Sam Slotsky <sam@manifold.co>
2019-06-27 23:25:04 -0500

    Remove unused route

--- src/components/app-root/app-root.tsx
+++ src/components/app-root/app-root.tsx
@@ -6,14 +6,6 @@ import { Component, h } from "@stencil/core";
 })
 export class AppRoot {
   render() {
-    return (
-      <ion-app>
-        <ion-router useHash={false}>
-          <ion-route url="/" component="app-home" />
-          <ion-route url="/profile/:name" component="app-profile" />
-        </ion-router>
-        <ion-nav />
-      </ion-app>
-    );
+    return <app-home />;
   }
 }
