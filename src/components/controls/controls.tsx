610f83b3d3fc6bc8f46bf337f23dec8d48b3ffb8
Sam Slotsky <sam@manifold.co>
2019-07-01 17:59:12 -0500

    Slide control menu in and out

--- src/components/controls/controls.tsx
+++ src/components/controls/controls.tsx
@@ -1,4 +1,4 @@
-import { Component, h, Prop } from "@stencil/core";
+import { Component, h, Prop, Host, State } from "@stencil/core";
 
 import Tunnel, {
   Effect,
@@ -15,6 +15,7 @@ export class WhatADrag {
   @Prop() dispatch: (action: ActionHandler) => void;
   @Prop() effect: Effect;
   @Prop() hue: number;
+  @State() expanded: boolean = true;
 
   setHue = (e: Event) => {
     const { value } = e.target as HTMLInputElement;
@@ -27,69 +28,76 @@ export class WhatADrag {
 
   render() {
     return (
-      <div
-        class="controls"
-        onMouseMove={e => e.stopPropagation()}
-        ref={node =>
-          node.addEventListener("touchmove", e => e.stopPropagation())
-        }
-      >
-        <div class="control">
-          <h2>Draw on me!</h2>
-          <p>
-            Try click-dragging to draw on the screen. If you have a touch
-            screen, try drawing with your finger!
-          </p>
-        </div>
-        <div class="control">
-          <h2>Choose your effect</h2>
-          <label>
+      <Host class={{ expanded: this.expanded, collapsed: !this.expanded }}>
+        <div
+          class="controls"
+          onMouseMove={e => e.stopPropagation()}
+          ref={node =>
+            node.addEventListener("touchmove", e => e.stopPropagation())
+          }
+        >
+          <div class="control">
+            <h2>Draw on me!</h2>
+            <p>
+              Try click-dragging to draw on the screen. If you have a touch
+              screen, try drawing with your finger!
+            </p>
+          </div>
+          <div class="control">
+            <h2>Choose your effect</h2>
+            <label>
+              <input
+                type="radio"
+                name="dot-type"
+                value="cruiser"
+                checked={this.effect === "fader"}
+                onClick={this.setEffect("fader")}
+              />{" "}
+              Fader
+            </label>
+            <label>
+              <input
+                type="radio"
+                name="dot-type"
+                value="cruiser"
+                checked={this.effect === "cruiser"}
+                onClick={this.setEffect("cruiser")}
+              />{" "}
+              Cruiser
+            </label>
+            <label>
+              <input
+                type="radio"
+                name="dot-type"
+                value="gravity"
+                checked={this.effect === "gravity"}
+                onClick={this.setEffect("gravity")}
+              />{" "}
+              Gravity
+            </label>
+          </div>
+          <div class="control">
+            <h2>Choose a color</h2>
             <input
-              type="radio"
-              name="dot-type"
-              value="cruiser"
-              checked={this.effect === "fader"}
-              onClick={this.setEffect("fader")}
-            />{" "}
-            Fader
-          </label>
-          <label>
-            <input
-              type="radio"
-              name="dot-type"
-              value="cruiser"
-              checked={this.effect === "cruiser"}
-              onClick={this.setEffect("cruiser")}
-            />{" "}
-            Cruiser
-          </label>
-          <label>
-            <input
-              type="radio"
-              name="dot-type"
-              value="gravity"
-              checked={this.effect === "gravity"}
-              onClick={this.setEffect("gravity")}
-            />{" "}
-            Gravity
-          </label>
+              type="range"
+              class="color-slider"
+              min={0}
+              max={360}
+              value={this.hue}
+              onInput={this.setHue}
+            />
+            <div
+              class="base-color"
+              style={{ "background-color": `hsla(${this.hue}, 100%, 50%, 1)` }}
+            />
+          </div>
         </div>
-        <div class="control">
-          <h2>Choose a color</h2>
-          <input
-            type="range"
-            class="color-slider"
-            min={0}
-            max={360}
-            value={this.hue}
-            onInput={this.setHue}
-          />
-          <div
-            class="base-color"
-            style={{ "background-color": `hsla(${this.hue}, 100%, 50%, 1)` }}
-          />
+        <div class="switch">
+          <button onClick={() => (this.expanded = !this.expanded)}>
+            &rarr;
+          </button>
         </div>
-      </div>
+      </Host>
     );
   }
 }
