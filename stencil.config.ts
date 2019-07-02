4bb2b9997927536ece7a077d0b344bb7c219f4de
Sam Slotsky <sslotsky@users.noreply.github.com>
2019-07-02 00:11:23 -0500

    Animate menu (#3)
    * Slide control menu in and out
    
    * Switch to different arrow

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,12 +1,29 @@
-import { Config } from '@stencil/core';
+import { Config } from "@stencil/core";
+import { postcss } from "@stencil/postcss";
+import postCSSPresetEnv from "postcss-preset-env";
 
 // https://stenciljs.com/docs/config
 
 export const config: Config = {
-  outputTargets: [{
-    type: 'www',
-    serviceWorker: null
-  }],
-  globalScript: 'src/global/app.ts',
-  globalStyle: 'src/global/app.css'
+  plugins: [
+    postcss({
+      plugins: [
+        postCSSPresetEnv({
+          features: {
+            "custom-media-queries": true,
+            "nesting-rules": true
+          }
+        })
+      ]
+    })
+  ],
+
+  outputTargets: [
+    {
+      type: "www",
+      serviceWorker: null
+    }
+  ],
+  globalScript: "src/global/app.ts",
+  globalStyle: "src/global/app.css"
 };
